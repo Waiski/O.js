f569662e6ca4960f7a06f555da25dd4ac5a096ed
Valtter Vihervuori <valtter@vihervuori.fi>
2016-01-31 21:03:47 +0200

    Some initial security rules.

--- app/server/rules.js
+++ app/server/rules.js
@@ -1,24 +1,52 @@
 var isAdmin = function(user) {
-    if (!user)
-        user = Meteor.user();
-    return Roles.userIsInRole(user, "admin");
+  if (!user)
+    user = Meteor.user();
+  return Roles.userIsInRole(user, "admin");
 };
 
+var isUser = function(userId) {
+  return _.isString(userId);
+}
+
+var no = function() {
+  return false;
+}
+
 Meteor.users.allow({
-    insert: function (userId, doc) {
-        return false;
-    },
-    update: function (userId, doc, fields, modifier) {
-        if (isAdmin(userId)) {
-            return true;
-        } else if (userId === doc._id) {
-            if (_.difference(fields, ['username', 'email']).length === 0) {
-                return true;
-            }
-        }
-        return false;
-    },
-    remove: function (userId, doc) {
-        return isAdmin(userId);
+  insert: function (userId, doc) {
+    return false;
+  },
+  update: function (userId, doc, fields, modifier) {
+    if (isAdmin(userId)) {
+      return true;
+    } else if (userId === doc._id) {
+      // Allow non-admins to only edit some fo their own info
+      if (_.difference(fields, ['username', 'email']).length === 0) {
+        return true;
+      }
     }
+    return false;
+  },
+  remove: isAdmin
+});
+
+Categories.allow({
+  insert: isUser,
+  update: isUser,
+  remove: isAdmin
+});
+
+Drinks.allow({
+  insert: isUser,
+  update: isUser,
+  remove: isUser
+});
+
+// Once the schema is set, this should be perhamps made more 
+// elaborate so that only standard transactions are allowed
+// for normal users.
+Transactions.allow({
+ insert: isUser,
+ update: isUser,
+ remove: isAdmin
 });
