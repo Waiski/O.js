f49bb6435b8877bdb6439110b5d2ac840fa44cc7
Valtter Vihervuori <valtter.vihervuori@atalent.fi>
2016-03-02 19:54:39 +0200

    Translitterate cyrillic names for searching.

--- app/collections/drinks.js
+++ app/collections/drinks.js
@@ -19,6 +19,26 @@ Drink.prototype = {
   }
 };
 
+// Remove cyrillic characters for searchable string
+// From: http://stackoverflow.com/questions/11404047/transliterating-cyrillic-to-latin-with-javascript-function
+var transliterate = function (word){
+  var charMap = { "Ё":"YO","Й":"I","Ц":"TS","У":"U",
+  "К":"K","Е":"E","Н":"N","Г":"G","Ш":"SH","Щ":"SCH",
+  "З":"Z","Х":"H","Ъ":"'","ё":"yo","й":"i","ц":"ts",
+  "у":"u","к":"k","е":"e","н":"n","г":"g","ш":"sh",
+  "щ":"sch","з":"z","х":"h","ъ":"'","Ф":"F","Ы":"I",
+  "В":"V","А":"a","П":"P","Р":"R","О":"O","Л":"L",
+  "Д":"D","Ж":"ZH","Э":"E","ф":"f","ы":"i","в":"v",
+  "а":"a","п":"p","р":"r","о":"o","л":"l","д":"d",
+  "ж":"zh","э":"e","Я":"Ya","Ч":"CH","С":"S","М":"M",
+  "И":"I","Т":"T","Ь":"'","Б":"B","Ю":"YU","я":"ya",
+  "ч":"ch","с":"s","м":"m","и":"i","т":"t","ь":"'",
+  "б":"b","ю":"yu" };
+  return word.split('').map(function (char) { 
+    return charMap[char] || char; 
+  }).join("");
+}
+
 var DrinkSchema = new SimpleSchema({
   name: {
     type: String,
@@ -45,7 +65,7 @@ var DrinkSchema = new SimpleSchema({
     autoValue: function() {
       var name = this.field('name');
       if (name.isSet)
-        return Diacritics.remove(name.value);
+        return transliterate(Diacritics.remove(name.value));
       else
         this.unset();
     }
